42fb3e190225185aa363fbc2a18e5881cf3b2d4d
Om Pandey <iamompandey.it@gmail.com>
2026-09-14 15:01:10 +0545

    aded  two model

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -1,3 +1,20 @@
+from users.models import UserProfile
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+
+class Gallery(models.Model):
+    GALLERY_TITLE = models.CharField(max_length=500)
+    GALLERY_IMAGE = models.ImageField(upload_to="Uploads/Gallary/")
+    GALLERY_DESCRIPTION = models.TextField()
+    GALLERY_CREATED_BY = models.ForeignKey(UserProfile, on_delete=models.PROTECT, related_name="GALLERY_CREATED_BY")
+    GALLERY_MODIFIED_BY = models.ForeignKey(UserProfile, on_delete=models.PROTECT, related_name="GALLERY_MODIFIED_BY")
+    GALLERY_CREATED_AT = models.DateTimeField(default=timezone.now)
+    GALLERY_MODIFIED_AT = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "GALLERY"
+
+    def __str__(self):
+        return self.GALLERY_TITLE
--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    USER_FULL_NAME = models.CharField(max_length=200)
+    USER_EMAIL = models.EmailField()
+    USER_MOBILE_NO = models.CharField(max_length=15)
+    USER_ADDRESS = models.TextField()
+    USER_IP = models.CharField(max_length=45)
+
+    USER_CREATED_BY = models.CharField(max_length=200, null=True, blank=True)
+    USER_MODIFIED_BY = models.CharField(max_length=200, null=True, blank=True)
+
+    USER_CREATED_AT = models.DateTimeField(default=timezone.now)
+    USER_UPDATED_AT = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "USER PROFILE"
+        verbose_name = "User Profile"
+        verbose_name_plural = "User Profiles"
+
+    def __str__(self):
+        return self.USER_FULL_NAME
+    
